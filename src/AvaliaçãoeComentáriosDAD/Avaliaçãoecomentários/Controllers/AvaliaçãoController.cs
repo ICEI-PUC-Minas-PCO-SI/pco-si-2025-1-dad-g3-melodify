60e82c8fe2846a6557e7e7deec515a401d31b2af
Daniel Vitor <danielsalviano879@gmail.com>
2025-05-31 16:51:24 -0300

    Correção final da avaliação e comentários

---
+++ src/AvaliaçãoeComentáriosDAD/Avaliaçãoecomentários/Controllers/AvaliaçãoController.cs
@@ -0,0 +1,61 @@
+﻿using Avaliaçãoecomentários.Data;
+using Avaliaçãoecomentários.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avaliaçãoecomentários.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AvaliacaoController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public AvaliacaoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Avaliação>>> Get()
+        {
+            return await _context.Avaliacoes.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Avaliação>> GetById(int id)
+        {
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+            if (avaliacao == null) return NotFound();
+            return avaliacao;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Avaliação>> Post(Avaliação avaliacao)
+        {
+            _context.Avaliacoes.Add(avaliacao);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = avaliacao.Id }, avaliacao);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Avaliação avaliacao)
+        {
+            if (id != avaliacao.Id) return BadRequest();
+            _context.Entry(avaliacao).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var avaliacao = await _context.Avaliacoes.FindAsync(id);
+            if (avaliacao == null) return NotFound();
+
+            _context.Avaliacoes.Remove(avaliacao);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ src/AvaliaçãoeComentáriosDAD/Avaliaçãoecomentários/Controllers/ComentárioController.cs
@@ -0,0 +1,61 @@
+﻿using Avaliaçãoecomentários.Data;
+using Avaliaçãoecomentários.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Avaliaçãoecomentários.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ComentarioController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ComentarioController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Comentario>>> Get()
+        {
+            return await _context.Comentarios.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Comentario>> GetById(int id)
+        {
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario == null) return NotFound();
+            return comentario;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Comentario>> Post(Comentario comentario)
+        {
+            _context.Comentarios.Add(comentario);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetById), new { id = comentario.Id }, comentario);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Comentario comentario)
+        {
+            if (id != comentario.Id) return BadRequest();
+            _context.Entry(comentario).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var comentario = await _context.Comentarios.FindAsync(id);
+            if (comentario == null) return NotFound();
+
+            _context.Comentarios.Remove(comentario);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
