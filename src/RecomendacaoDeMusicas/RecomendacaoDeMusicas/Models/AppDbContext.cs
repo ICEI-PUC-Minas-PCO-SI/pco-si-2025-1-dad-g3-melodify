3b3b806a7db5d7be78a7ab3610a77903e9bfbd13
Carol <carolinaabpessoa@gmail.com>
2025-06-01 13:32:15 -0300

    criando estrutura de musicas e modelo de dados

---
+++ src/RecomendacaoDeMusicas/RecomendacaoDeMusicas/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RecomendacaoDeMusicas.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Musica> Musicas { get; set; }
+    }   
+}
---
+++ src/RecomendacaoDeMusicas/RecomendacaoDeMusicas/Models/Musica.cs
@@ -0,0 +1,11 @@
+﻿namespace RecomendacaoDeMusicas.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public string Genero { get; set; }
+        public int AnoLancamento { get; set; }
+    }
+}
