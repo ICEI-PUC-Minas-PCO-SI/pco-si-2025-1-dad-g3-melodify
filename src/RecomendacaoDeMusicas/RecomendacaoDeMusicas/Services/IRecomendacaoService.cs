4fb150f6745ca14d560dd7c610da29c5589f3570
Carol <carolinaabpessoa@gmail.com>
2025-06-01 16:41:39 -0300

    criando services para fazer conexão com banco

---
+++ src/RecomendacaoDeMusicas/RecomendacaoDeMusicas/Services/IRecomendacaoService.cs
@@ -0,0 +1,11 @@
+﻿using RecomendacaoDeMusicas.Models;
+
+namespace RecomendacaoDeMusicas.Services
+{
+    public interface IRecomendacaoService
+    {
+        IEnumerable<Musica> GetByGenero(string genero);
+        IEnumerable<Musica> GetByAno(int ano);
+        IEnumerable<Musica> GetByArtista(string artista);
+    }
+}
